fa3d96a49d234a7a088024e9a5cc6ddbd33582b5
gusti126 <gustidoang936@gmail.com>
2021-08-12 16:53:57 +0700

    create migration blogs, komentars, reply_komentar

--- database/migrations/2021_08_12_095122_create_reply_komens_table.php
+++ database/migrations/2021_08_12_095122_create_reply_komens_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateReplyKomensTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reply_komens', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('komentar_id')->constrained('komentars')->onDelete('cascade');
+            $table->longText('body');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reply_komens');
     }
 }
